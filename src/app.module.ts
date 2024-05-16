54515e2419376742f6d27719934e171aa05c0c26
Pavlo Vlasiuk <113940774+PavloVlasiuk@users.noreply.github.com>
2024-05-17 02:21:58 +0300

    feat: implemented endpoint to get usd to uah exchange rate by NBU (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { RateModule } from './rate/rate.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RateModule],
 })
 export class AppModule {}
