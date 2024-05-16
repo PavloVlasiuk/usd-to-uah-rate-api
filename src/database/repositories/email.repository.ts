168bc7dcec50d75f477ec70744dbecd468e477c8
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-05-16 23:08:52 +0300

    feat: implemented database module with email repository

---
+++ src/database/repositories/email.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Email, Prisma } from '@prisma/client';
+
+@Injectable()
+export class EmailRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.EmailCreateInput): Promise<Email> {
+    return this.prisma.email.create({ data });
+  }
+}
