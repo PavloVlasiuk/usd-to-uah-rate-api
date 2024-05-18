058dafe7266b0a8bce080024d96a067f89489ab0
Pavlo Vlasiuk <113940774+PavloVlasiuk@users.noreply.github.com>
2024-05-19 01:45:25 +0300

    feat: added validation for subscribeEmailDto (#6)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app-config.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,16 @@ async function bootstrap() {
   const GLOBAL_PREFIX = appConfigService.get<string>('app.globalPrefix');
 
   app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(PORT, HOST, async () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
